ff2cfd767b4f352ea27258bf7c66f3174947faad
sherlyazkiah <sherlyazkiah12@gmail.com>
2025-11-16 16:30:49 +0700

    W12: Answer to Question 4

--- codelab12_stream/stream_sherly/lib/main.dart
+++ codelab12_stream/stream_sherly/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'stream.dart';
 
 void main() {
   runApp(const MyApp());
@@ -27,8 +28,33 @@ class StreamHomePage extends StatefulWidget {
 }
 
 class _StreamHomePageState extends State<StreamHomePage> {
+  Color bgColor = Colors.blueGrey;
+  late ColorStream colorStream;
+
+  @override
+  void initState() {
+    super.initState();
+    colorStream = ColorStream();
+    changeColor();
+  }
+
+  void changeColor() async {
+    await for (var eventColor in colorStream.getColorStream()) {
+      setState(() {
+        bgColor = eventColor;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Stream - Sherly'),
+      ),
+      body: Container(
+        decoration: BoxDecoration(color: bgColor),
+      ),
+    );
   }
 }
