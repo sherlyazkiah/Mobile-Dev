3cdffcd1ec3f6ac5482b96faaad6732c74f5bd5e
sherlyazkiah <sherlyazkiah12@gmail.com>
2025-11-16 16:09:40 +0700

    W12: Answer to Question 3

--- codelab12_stream/stream_sherly/lib/stream.dart
+++ codelab12_stream/stream_sherly/lib/stream.dart
@@ -13,4 +13,12 @@ class ColorStream {
     Colors.pink,
     Colors.cyan,
   ];
+
+  Stream<Color> getColorStream() async* {
+    yield* Stream.periodic(
+      const Duration(seconds: 1), (int t) {
+        int index = t % colors.length;
+        return colors[index];
+    });
+  }
 }
